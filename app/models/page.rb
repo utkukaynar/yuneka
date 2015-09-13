70a3ce450c0723281c51219672e26e58bec6a32e
Utku Kaynar <utkukaynar@gmail.com>
2015-09-13 19:46:00 +0300

    Added attr_accessor to page model, necessary for CK Editor.

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,5 @@
 class Page < ActiveRecord::Base
+  attr_accessor :image_file_name, :image_content_type, :image_file_size, :image_updated_at
   has_attached_file :image, 
     styles: { large: "600x600>", medium: "400x400>", thumb: "100x100>" }, 
     default_url: "/images/:style/missing.png"
