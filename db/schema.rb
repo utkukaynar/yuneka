1057ed81fef74cac2cc997599bc1d79b3888354e
Utku Kaynar <utkukaynar@gmail.com>
2015-09-13 15:55:13 +0300

    Added a products table with category foreign key.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150913123649) do
+ActiveRecord::Schema.define(version: 20150913125139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,21 @@ ActiveRecord::Schema.define(version: 20150913123649) do
     t.datetime "updated_at",       null: false
   end
 
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "unit"
+    t.integer  "category_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+  end
+
+  add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
+
   create_table "vendors", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
@@ -77,4 +92,5 @@ ActiveRecord::Schema.define(version: 20150913123649) do
     t.datetime "image_updated_at"
   end
 
+  add_foreign_key "products", "categories"
 end
