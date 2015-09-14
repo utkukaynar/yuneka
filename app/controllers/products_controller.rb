5f88feffef9e8f4187aed96bae69d20ff454cafe
Utku Kaynar <utkukaynar@gmail.com>
2015-09-14 22:35:57 +0300

    Final commit, slightly added product show view.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
   def show
-    
+    @product = Product.find(params[:id])
+    @category = @product.category
+    @vendor = @category.vendor
   end
 end
