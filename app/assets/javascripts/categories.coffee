27a3401767abca7f14a985b9480cdc0a75047a51
Utku Kaynar <utkukaynar@gmail.com>
2015-09-14 18:13:34 +0300

    added necessary show views and completed a product line.

--- app/assets/javascripts/categories.coffee
+++ app/assets/javascripts/categories.coffee
@@ -1,3 +1,10 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+$(document).ready ->
+  $('.panel-collapse').on('show.bs.collapse', ->
+    $(this).parent().find('.glyphicon-menu-right').removeClass('glyphicon-menu-right').addClass 'glyphicon-menu-down'
+    return
+  ).on 'hide.bs.collapse', ->
+    $(this).parent().find('.glyphicon-menu-down').removeClass('glyphicon-menu-down').addClass 'glyphicon-menu-right'
+    return
+  return
+    
+    
--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,11 @@ Rails.application.routes.draw do
   get '/vendors/:vendor_id/categories', to: 'categories#index', as: 'categories'
   get '/vendors/:vendor_id/categories/:id', to: 'categories#show', as: 'category'
   # get '/vendors/:vendor_id/categories/:category_id/products', to: 'products#index', as: 'products'
-  get '/vendors/:vendor_id/categories/:category_id/products/:id', to: 'products#show', as: 'product'
+  # get '/vendors/:vendor_id/categories/:category_id/products/:id', to: 'products#show', as: 'product'
   root "welcome#index"
   get '/posts/:id', to: 'posts#show', as: 'post'
   get '/posts', to: 'posts#index'
   get '/pages/:id', to: 'pages#show', as: 'page'
   get '/pages', to: 'pages#index'
+  get '/products/:id', to: 'products#show', as: 'product'
 end
