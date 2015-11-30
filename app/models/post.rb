3907bf3d5c85674cb39d6987a3c5edd9632b783b
Utku Kaynar <utkukaynar@gmail.com>
2015-11-30 22:32:19 +0200

    Post model modif for attr accessor.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ActiveRecord::Base
   enum type: [:article, :announcement]
-  has_attached_file :image, 
-    styles: { large: "600x600>", medium: "400x400>", thumb: "100x100>" }, 
+  has_attached_file :image,
+    styles: { large: "600x600>", medium: "400x400>", thumb: "100x100>" },
     default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: ['image/jpeg', 'image/jpg', 'image/png']
+  attr_accessor :image_file_name, :image_content_type, :image_file_size, :image_updated_at
 end
